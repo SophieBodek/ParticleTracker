a3b3076945a074c52e28c67cd2155a9e1e89694a
SophieBodek <118883849+SophieBodek@users.noreply.github.com>
2025-07-24 14:02:59 -0700

    Update readme for tiff read/view capabilities

--- PlotTracksExample.m
+++ PlotTracksExample.m
@@ -1,20 +1,52 @@
-% PlotTracks.m, written July 2025 by Sophie Bodek, provides code that can
-% visualize the vtracks output of the PredictiveTracker.m function using
-% different methods, such as the longtracks.m function or
-% PredTrackerMovie.m function. 
+% PlotTracks.m provides example code to visualize the vtracks
+% output of the PredictiveTracker.m function using built-in plotting
+% functionalities of the Velocities.m, Vorticities.m, and Divergences.m 
+% functions. Additional 
 % 
 % In order to run, this does require the following:
-%   -   bigread4.m : quickly loads large .TIFF stacks
-%   -   Velocities.m : outputs X, Y, T, U, V, Tr from vtracks
+%   -   Velocities.m : outputs [X, Y, T, U, V, Tr] from vtracks
 %                      also plots particle velocity vectors
-%   -   TiffTrackViewer.m : viewer for playing images as a movie with tracks overlaid
+%   -   Vorticities.m : outputs [Vort, X, Y, T, Tr] from vtracks
+%                       also plots particle vorticities
+%   -   Divergences.m : outputs [Div, X, Y, T, Tr] from vtracks
+%                       also plots particle divergences
 %   -   longtracks.m : plots tracks over a given length
+%   -   bigread4.m : quickly loads large .TIFF stacks
+%   -   TiffTrackViewer.m : viewer for playing images as a movie with tracks overlaid
 %   -   PredTrackerMovie.m : plays active tracks over images as a movie --> TO ADD MAYBE
 
 close all
 clear all
 
-%% %--------- FOLDERS AND FILENAMES
+% Load vtracks structure (assumes you have already run PredictiveTracker.m elsewhere)
+load('vtracks.mat');
+
+% Use built-in plotting functionality of Velocities, Vorticities, and Divergences 
+% to visualize particles-----------------------------------------------------------------
+framerange=[1 inf];
+noisy = 1; %plotting parameter : no plot = 0; plot velocity vectors = 1
+
+disp('Velocities : plots particle velocity vectors');
+[u,v,x,y,t,tr]=Velocities(vtracks,framerange,noisy);
+
+disp('Vorticities: plots particle vorticities')
+%[vort,~,~,~,~]=Vorticities(vtracks,framerange,noisy) %need to fix old vorticities code
+
+disp('Divergences: plots particle divergences')
+%[div,~,~,~,~]=Divergences(vtracks,framerange,noisy) %need to fix old divergences code
+
+% Use longtracks to view tracks over minlength -------------------------------------------
+tracks = sortrows([tr x y], 1); %need to sort data due to how function is written
+minlength = 50; %minimum track length to plot
+
+fprintf('longtracks : Plots all tracks longer than %d frames.\n', minlength);
+[nlong,ntracks,lmean,lrms,lmax]=longtracks(tracks,minlength,noisy);
+
+% Tiff player with functionality for viewing overlaid tracks -----------------------------
+% Add field for track index (helps with referencing track number in original vtracks struct)
+ids = num2cell(1:length(vtracks));
+[vtracks.ID] = deal(ids{:});
+
 % Are the images saved as a .TIFF stack? or as a series of .TIFF files?
 isStack = 2; %0 for invidiual images in a directory; 1 for .TIFF stack; 2 for both
 
@@ -34,28 +66,6 @@ if isStack ==1 || isStack ==2     %--------- .TIFF stack
     TS = bigread4(stack_name, tiff_frame_start, tiff_num_frames); %load .TIFF stack
 end
 
-%--------- Load vtracks 
-% (assumes you have already run PredictiveTracker elsewhere)
-load('vtracks.mat');
-
-% Add field for track index (helps with referencing track number in original vtracks struct)
-ids = num2cell(1:length(vtracks));
-[vtracks.ID] = deal(ids{:});
-
-%--------- Calculate velocities
-framerange=[1 inf];
-noisy = 1; %plotting parameter : no plot = 0; plot velocity vectors = 1
-% Note: Velocities.m also has a visualization tool (noisy = 1) that plots
-% the velocity vector of each patricle
-disp('Velocities : plots particle velocity vectors');
-[u,v,x,y,t,tr]=Velocities(vtracks,framerange,noisy);
-
-%fig_velocities = gcf; %save velocities figure if desired
-%saveas(fig_velocities,[directory filesep 'velocities.png']); 
-
-disp('Tracks locked & loaded!')
-
-%% Visualization Method 1: TIFF TRACK VIEWER
 % Displays viewer with intuitive controls for playing images as a movie and
 % overlaying tracks, as well as plotting trajectory metrics
 n_ch = 1; %number of channels (1 if greyscale; 3 if color)
@@ -67,19 +77,6 @@ if isStack == 0 %functionality more limited for directory with images
 else %otherwise should display .TIFF stack
     tv = TiffTrackViewer(TS,n_ch,vtracks,minlen); 
 end
-
-%% Visualization Method 2: Long Tracks
-% This function plots all tracks over a given length
-tracks = sortrows([tr x y], 1); %need to sort data due to how function is written
-noisy = 1; %display plot
-minlength = 50; %minimum track length to plot
-readwhole = 0; %'True';
-
-fprintf('longtracks : Plots all tracks longer than %d.\n', minlength);
-[nlong,ntracks,lmean,lrms,lmax]=longtracks(tracks,minlength,noisy);
-
-%fig_longtracks = gcf; %to save the figure
-%saveas(fig_longtracks,[directory filesep 'longtracks.png']); 
 
 %% Visualization Method 3: Predictive Tracker Movie
 % This function plays the same movie as the PredictiveTracker function
